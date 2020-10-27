b063aa79ca311351c5be4017311548ecba53d4bc
Dimpal <dimpal.agrawal_cs19@gla.ac.in>
2020-10-27 12:35:16 +0530

    display number of days left in months by declaring a cases by using switch cases

--- src/Months.java
+++ src/Months.java
@@ -1,10 +1,11 @@
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
+import java.util.Scanner;
 
 public enum Months {
     JANUARY(31),
-    FEBRUARY(29),
+    FEBRUARY(28),
     MARCH(31),
     APRIL(30),
     MAY(31),
@@ -28,12 +29,92 @@ public enum Months {
 }
 
 class Main {
+    private static final int JANUARY = 0;
+    private static final int FEBRUARY = 1;
+    private static final int MARCH = 2;
+    private static final int APRIL = 3;
+    private static final int MAY = 4;
+    private static final int JUNE = 5;
+    private static final int JULY = 6;
+    private static final int AUGUST = 7;
+    private static final int SEPTEMBER = 8;
+    private static final int OCTOBER = 9;
+    private static final int NOVEMBER = 10;
+    private static final int DECEMBER = 11;
+
     public static void main(String[] args) {
         int currentMonth = new GregorianCalendar().get(Calendar.MONTH);
         System.out.println(currentMonth);
         System.out.println(new GregorianCalendar().getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH));
         int currentDate = new GregorianCalendar().get(Calendar.DATE);
         System.out.println(currentDate);
+        Scanner scanner = new Scanner(System.in);
+        int numberOfDays;
+        int numberOfDaysLeftInMonths;
+        switch (currentMonth) {
+            case JANUARY:
+                numberOfDays = Months.JANUARY.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case FEBRUARY:
+                numberOfDays = Months.FEBRUARY.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case MARCH:
+                numberOfDays = Months.MARCH.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case APRIL:
+                numberOfDays = Months.APRIL.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case MAY:
+                numberOfDays = Months.MAY.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case JUNE:
+                numberOfDays = Months.JUNE.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case JULY:
+                numberOfDays = Months.JULY.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case AUGUST:
+                numberOfDays = Months.AUGUST.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case SEPTEMBER:
+                numberOfDays = Months.SEPTEMBER.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case OCTOBER:
+                numberOfDays = Months.OCTOBER.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case NOVEMBER:
+                numberOfDays = Months.NOVEMBER.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            case DECEMBER:
+                numberOfDays = Months.DECEMBER.getNumberOfDays();
+                numberOfDaysLeftInMonths = numberOfDays - currentDate;
+                System.out.println(numberOfDaysLeftInMonths);
+                break;
+            default:
+                System.out.println("Have a nice day");
+        }
 
     }
 }
